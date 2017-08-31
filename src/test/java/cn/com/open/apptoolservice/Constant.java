900abfdf040b1d0eb80f705654f7d6eb54dc29af
wangshuaia <wangshuaia@wangshuaia-PC>
2017-08-31 10:35:43 +0800

    添加测试工具类

--- src/test/java/cn/com/open/apptoolservice/Constant.java
+++ src/test/java/cn/com/open/apptoolservice/Constant.java
@@ -4,7 +4,7 @@ public class Constant {
 
     //public static final String BASEURL = "http://localhost:8080/api/apptoolservice/v1";
     //public static final String BASEURL = "http://apptool.testyf.ak:8042/api/apptoolservice/v1";
-    public static final String APPKEY = "wkdsjdhshoewihoi2ioio34i";
-    public static final String APPSECRET = "2kwi3k9skskdlf2sssss";
+    public static final String APPKEY = "08d30a26beec4d2996fa0581abbadbea";
+    public static final String APPSECRET = "eae34a4c38f94aa4a3bb01b4ad821a57";
 
 }
